a324a30aa5adbc01cd070dc6c49e49f3053d823a
Onur Ucar <onurucar@iu.edu>
2021-01-18 23:55:27 -0500

    js

---
+++ Web Developer/14- Javascript Basics/primitive.js
@@ -0,0 +1,20 @@
+/*      PRIMITIVE DATA TYPES    */
+var newNum = 1;
+var isTrue = true;
+var name = "Onur";
+typeof(name);                    // String
+
+0/0;                             // NaN
+1+NaN;                           // NaN
+
+
+/*      VARIABLES & LET         */
+let someName = "Galis";
+let year = 1943;
+
+/*      CONST & VAR             */
+// const works just like let, except you cannot change the value!
+const hens = 4;
+//hens = 20;                  // NO! Error!
+// We used to use var, in good old days :(
+    
