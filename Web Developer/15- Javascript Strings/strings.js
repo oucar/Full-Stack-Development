95e5a5fc5b268664ad7060e61eb3eb5679cbf988
oucar <hello@ucaronur.com>
2021-01-18 23:55:27 -0500

    js

---
+++ Web Developer/15- Javascript Strings/strings.js
@@ -0,0 +1,25 @@
+//  thing.property;
+//  thing.method();
+
+let animal = "dog"; //console.log(animal[0]) --> d
+animal.length;      // 3;
+"lol" + "lol"; // lollol --> string concatenation
+
+
+/*
+    .toUpperCase();
+    .toLowerCase();
+    .trim()               // trims the whitespace!
+    .indexOf("a");        // returns the index of first occurence of "a".
+    .slice(0,3);          // between 0 and 3
+    .slice(5);            // starting from 5 to all the way to end
+    .replace('onur', 'haha');   // replaces onur with haha
+*/
+
+let str = `I counted ${3+4} sheep`;             // BACKTICK!            --> String literal
+Math.floor(Math.random() * 10);     // returns a random integer from 0 to 9 
+
+/* 
+NULL:       Intentional absence of any value, must be assigned.
+undefined:  Variables that do not have an assigned value are undefined
+*/
