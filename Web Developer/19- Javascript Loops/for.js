3010fd6923c65e35e7bd6b95d554de0357a71a90
oucar <hello@ucaronur.com>
2021-01-20 01:54:20 -0500

    js

---
+++ Web Developer/19- Javascript Loops/for.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 10; i++) {
+    console.log(i + 1 + " chicken");
+}
+
+//print even numbers
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// or
+
+for (let i = 0; i <= 20; i+=2) {
+    console.log(i);
+}
+
---
+++ Web Developer/19- Javascript Loops/loopingOverObjects.js
@@ -0,0 +1,38 @@
+// key - value pairs are not iterable
+// but you can use FOR IN!
+
+
+// did you notice the difference between for of and for in?
+// you use FOR OF in arrays but you use FOR IN in objects
+
+const testScores = {
+    a: 20,
+    b: 30,
+    c: 10,
+    d: 31,
+    e: 86,
+    f: 75
+}
+
+for(let person in testScores){
+    console.log(`${person} scored ${testScores[person]}`)
+}
+
+// But you can also use; AND THIS ONE IS BETTER!
+
+Object.keys(testScores);    // returns keys AS ARRAY
+Object.values(testScores);  // returns values AS ARRAY
+Object.entries(testScores); // key value pairs
+
+
+
+Object.entries(testScores)[0][0];       // a 
+
+
+let total = 0;
+let scores = Object.values(testScores);
+for(let score of scores){
+    total += score; 
+}
+
+console.log(`Average: ${total/(scores.length)}`);
