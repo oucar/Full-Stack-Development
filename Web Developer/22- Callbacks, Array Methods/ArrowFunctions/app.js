bc0ae59d09b3729e4379a966348c701f0eba9917
oucar <hello@ucaronur.com>
2021-01-24 02:22:06 -0500

    js

---
+++ Web Developer/22- Callbacks, Array Methods/ArrowFunctions/app.js
@@ -0,0 +1,65 @@
+// const add = function(x,y) {
+//     return x + y;
+// }
+
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+// a and b parameters
+const add = (a, b) => a + b;
+add(1,2);
+
+// num is the parameter
+const square = num => {
+    return num * num;
+}
+
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1
+// }
+
+
+// Since it's only one line, you can get rid of the return value BUT MAKE SURE YOU CHANGE CURLY BRACES TO REGULAR BRACES!!!!
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+// const newMovies = movies.map(function (movie) {
+//     return `${movie.title} - ${movie.score / 10}`
+// })
+
+
+// IMPLICIT RETURN (CHECK THE PHARENTATESSI!) ----> ONLY IF THERE'S ONE LINE TO RETURN
+// You use arrow function when passing something to another thing using a function
+const newMovies = movies.map(movie => (
+    `${movie.title} - ${movie.score / 10}`
+))
+
+
+const add2 = (a,b) => a+b; 
+
+
+
+
