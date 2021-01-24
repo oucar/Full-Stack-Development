9de9280e6e28fd889b42aecbc216363d2177ecff
Onur Ucar <onurucar@iu.edu>
2021-01-24 02:22:06 -0500

    js

---
+++ Web Developer/22- Callbacks, Array Methods/Map/app.js
@@ -0,0 +1,33 @@
+// Creates a new array with the results of calling a callback on every element in the array
+// This is an array callback method, so the argument represents every single element in the given array!
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const doubles = numbers.map(function (num) {
+    return num * 2;
+})
+
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+
+const titles = movies.map(function (movie) {
+    return movie.title.toUpperCase();
+})
+
