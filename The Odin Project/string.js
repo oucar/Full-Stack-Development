f1cc7adeac880c0e085d0b66c9a1268f97a2ed00
Onur Ucar <onurucar@iu.edu>
2021-02-04 02:26:32 -0500

    js

---
+++ The Odin Project/string.js
@@ -0,0 +1,26 @@
+// Escaping characters in a string:
+bigmouth = 'I\'ve got no right to take my place...';
+
+// int to str
+let myDate = '19' + '67';
+typeof myDate;
+
+let myNum2 = 123;
+let MyString2 = myNum2.toString();
+
+// str to int! 
+let myString = '123';
+let myNum = Number(myString);
+
+// Template Literals (back ticks)
+output = `I like the song "${ song }". I gave it a score of ${ score/highestScore * 100 }%.`;
+
+// LENGTH
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length; 
+
+// FINDING STRING IN A STRING
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("locate");
+var pos2 = str.search("locate");
+
