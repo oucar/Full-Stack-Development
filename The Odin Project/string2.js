7827e8f92eb718e9ff945ae8639043935ef58b85
oucar <hello@ucaronur.com>
2021-02-05 02:42:23 -0500

    js

--- The Odin Project/string2.js
+++ The Odin Project/string2.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Replace (only the first match)
 
 str = "Please visit Microsoft!";
@@ -53,3 +54,30 @@ txt.split(" ");          // Split on spaces
 txt.split("|");          // Split on pipe 
  var txt = "Hello";       // String
 txt.split("");           // Split in characters 
+=======
+// Escaping characters in a string:
+bigmouth = 'I\'ve got no right to take my place...';
+
+// int to str
+let myDate = '19' + '67';
+typeof myDate;
+
+let myNum2 = 123;
+let MyString2 = myNum2.toString();
+
+// str to int! 
+let myString = '123';
+let myNum = Number(myString);
+
+// Template Literals (back ticks)
+output = `I like the song "${ song }". I gave it a score of ${ score/highestScore * 100 }%.`;
+
+// LENGTH
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length; 
+
+// FINDING STRING IN A STRING
+var str = "Please locate where 'locate' occurs!";
+var pos = str.indexOf("locate");
+var pos2 = str.search("locate");
+
