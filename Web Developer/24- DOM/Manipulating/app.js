51195ee6d3839bc2c36c895d19f2db3e8cfadaaa
Onur Ucar <onurucar@iu.edu>
2021-02-14 00:41:06 -0500

    dom halfway

--- Web Developer/24- DOM/Manipulating/app.js
+++ Web Developer/24- DOM/Manipulating/app.js
@@ -1,12 +1,34 @@
-const allLinks = document.querySelectorAll('a');
+/*
+innerText returns the visible text contained in a node, while textContent returns the full text. For example, on the following HTML <span>Hello <span style="display: none;">World</span></span>, innerText will return 'Hello',
+while textContent will return 'Hello World'.
+*/
+
 
 // for (let link of allLinks) {
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
 
+
+// get, set attribute
+const firstLink = document.querySelector('a');
+firstLink.getAttribute('href');
+firstLink.setAttribute('href', 'http://www.google.com');
+
+const weirdInput = document.querySelector('input[type="text"]');
+
+
+
+// CHANGING STYLES
+const h1 = document.querySelector('h1');
+h1.setAttribute('style', 'color:tomato;');
+
+    // or
+
+const allLinks = document.querySelectorAll('a');
+
 for (let link of allLinks) {
     link.style.color = 'rgb(0, 108, 134)';
     link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
+    link.style.textDecorationStyle = 'wavy';
 }
