75cdb4c4587b2d849507cc90073e3d9857fa06d2
oucar <hello@ucaronur.com>
2021-02-14 00:41:06 -0500

    dom halfway

--- Web Developer/24- DOM/Selecting/app.js
+++ Web Developer/24- DOM/Selecting/app.js
@@ -1,8 +1,8 @@
 // Document is automatically created by browser!
-
+// tagName and Classes are HTML Collections and not an array. You can use for of though.
 
 // ID 
-const title = document.getElementById('silkieChickens');
+const title = document.getElementById('silkieChickens');    
 title.textContent = "Silkie Chinckens using getElementById();";
 
 
@@ -20,10 +20,20 @@ for (let img of squareImages) {
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
 }
 
-// TAG NAME (does the same with getElementByTagname)
-const links = document.querySelectorAll('p a');
+// QUERY SELECTOR(does the same with getElementByTagname)
+const links = document.querySelectorAll('p a'); // anchor tags in p
 
 for (let link of links) {
     console.log(link.href)
 }
+
+// QUERY SELECTOR TO nth-of-type(2)
+const secondImg = document.querySelector('img:nth-of-type(2)');
+secondImg.setAttribute('style', 'border: 25px solid tomato;');    
+
+
+// SELECTING BY ATTRIBUTE
+const java = document.querySelector('a[title="Java"]');
+java.setAttribute('style', 'color:red;')
+
 
