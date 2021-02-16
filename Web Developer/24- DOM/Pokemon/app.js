5fa170742a83ab035b66e686d18456922e6385af
oucar <hello@ucaronur.com>
2021-02-16 02:23:03 -0500

    event listeners

--- Web Developer/24- DOM/Pokemon/app.js
+++ Web Developer/24- DOM/Pokemon/app.js
@@ -7,8 +7,10 @@ const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprite
 for (let i = 1; i <= 151; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
+
     const label = document.createElement('span');
     label.innerText = `#${i}`;
+    
     const newImg = document.createElement('img');
     newImg.src = `${baseURL}${i}.png`
 
