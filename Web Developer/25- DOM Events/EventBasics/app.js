2aa6f230c359fbaa034b415cb4f75a8c32062fb3
Onur Ucar <onurucar@iu.edu>
2021-02-17 02:57:39 -0500

    event listeners

--- Web Developer/25- DOM Events/EventBasics/app.js
+++ Web Developer/25- DOM Events/EventBasics/app.js
@@ -1,15 +1,19 @@
+// VERSION 2
 const btn = document.querySelector('#v2');
 
+    // do this if btn is clicked
 btn.onclick = function () {
     console.log("YOU CLICKED ME!")
     console.log("I HOPE IT WORKED!!")
 }
+
 
 function scream() {
     console.log("AAAAAHHHHH");
     console.log("STOP TOUCHING ME!")
 }
 
+    // do this if btn is being hovered
 btn.onmouseenter = scream;
 
 
@@ -18,10 +22,20 @@ document.querySelector('h1').onclick = () => {
 }
 
 
-const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
+// VERSION 3, THE BEST!
+const btn3 = document.querySelector('#v3');     // SELECT
+btn3.addEventListener('click', function () {    // event that is being listened, callback function
     alert("CLICKED!");
 })
+
+const btn4 = document.querySelector("#v4");
+btn4.addEventListener('click', () => {
+    console.log("4TH ONE IS CLICKED!")
+});
+
+
+
+    // WHY DO WE USE EVENT LISTENERS?
 
 function twist() {
     console.log("TWIST!")
@@ -33,7 +47,10 @@ function shout() {
 const tasButton = document.querySelector('#tas');
 
 // tasButton.onclick = twist;
-// tasButton.onclick = shout;
+// tasButton.onclick = shout;       // OVERWRITTING TWIST!
 
 tasButton.addEventListener('click', twist)
 tasButton.addEventListener('click', shout)
+
+
+
