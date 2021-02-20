85243dde6ae946d1382c7ff18b702518a77339ac
oucar <hello@ucaronur.com>
2021-02-20 03:48:03 -0500

    dom

--- Web Developer/25- DOM Events/5- FormEvents/app.js
+++ Web Developer/25- DOM Events/5- FormEvents/app.js
@@ -1,15 +1,22 @@
 // ! Important!
+// Defaults behaviour submits the web page if user clicks on the button
 
 const tweetForm = document.querySelector('#tweetForm');
 const tweetsContainer = document.querySelector('#tweets');
+
 tweetForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
     // const usernameInput = document.querySelectorAll('input')[0];
     // const tweetInput = document.querySelectorAll('input')[1];
-    const usernameInput = tweetForm.elements.username;
-    const tweetInput = tweetForm.elements.tweet;
+
+    // ! tweetForm.elements.propertyName -> make sure you give them name
+    const usernameInput = tweetForm.elements.username;          // name = username
+    const tweetInput = tweetForm.elements.tweet;                // name = tweet
+
     addTweet(usernameInput.value, tweetInput.value)
+    
+    // clear the input fields
     usernameInput.value = '';
     tweetInput.value = '';
 });
@@ -17,9 +24,16 @@ tweetForm.addEventListener('submit', function (e) {
 const addTweet = (username, tweet) => {
     const newTweet = document.createElement('li');
     const bTag = document.createElement('b');
-    bTag.append(username)
-    newTweet.append(bTag);
-    newTweet.append(`- ${tweet}`)
+
+    bTag.append(username)                   // insert username to bTag
+    newTweet.append(bTag);                  // insert bTag to newTweet
+    newTweet.append(`- ${tweet}`)           // ! modifies itself
+
     tweetsContainer.append(newTweet);
 }
 
+
+
+
+
+// ? EXAMPLE
