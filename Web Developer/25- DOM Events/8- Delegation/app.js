8fa917aea1e6170bd573e4241265c4efd82431ad
oucar <hello@ucaronur.com>
2021-02-25 02:17:33 -0500

    event delegation

--- Web Developer/25- DOM Events/8- Delegation/app.js
+++ Web Developer/25- DOM Events/8- Delegation/app.js
@@ -1,25 +1,30 @@
 
 const tweetForm = document.querySelector('#tweetForm');
 const tweetsContainer = document.querySelector('#tweets');
+
 tweetForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const usernameInput = tweetForm.elements.username;
     const tweetInput = tweetForm.elements.tweet;
+
     addTweet(usernameInput.value, tweetInput.value)
     usernameInput.value = '';
     tweetInput.value = '';
+
 });
+
 
 const addTweet = (username, tweet) => {
     const newTweet = document.createElement('li');
-    const bTag = document.createElement('b');
+    const bTag = document.createElement('b');                       // use append to add a new element
     bTag.append(username)
     newTweet.append(bTag);
     newTweet.append(`- ${tweet}`)
     tweetsContainer.append(newTweet);
 }
 
+
 tweetsContainer.addEventListener('click', function (e) {
-    e.target.nodeName === 'LI' && e.target.remove();
+    e.target.nodeName === 'LI' && e.target.remove();                // ! short-hand if (so paragraph will not be deleted)
 })
 
