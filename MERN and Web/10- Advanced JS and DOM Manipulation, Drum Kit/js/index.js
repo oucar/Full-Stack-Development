92c01b81e0ff612c7c385556ab3e96bd1f6f7942
Onur Ucar <onurucar@iu.edu>
2021-02-28 04:43:38 -0500

    advanced functions

--- MERN and Web/10- Advanced JS and DOM Manipulation, Drum Kit/js/index.js
+++ MERN and Web/10- Advanced JS and DOM Manipulation, Drum Kit/js/index.js
@@ -1,7 +1,30 @@
+/* //!passing function as argument
+
+function add(num1, num2){
+    return num1 * num2;
+} // ? end add
+
+function calculator(num1, num2, operator){         //! This is a higher order function (can take other functions as inputs!)
+    return operator(num1, num2);
+} // ? end calculator
+
+calculator(4,5,add);
+*/
+
+
+
 const btns = document.querySelectorAll('button');
 
 for(btn of btns){
     btn.addEventListener('click', function(){
-        alert('I got clicked.');
+        
+        console.log(this);                          // ! do not use btn!
+
+
     });
 } // end for
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
