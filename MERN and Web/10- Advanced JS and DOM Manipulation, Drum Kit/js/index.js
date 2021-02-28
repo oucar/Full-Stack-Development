d4062d2c5e2ccfec4132dc7d7b7c72b24c3f8e44
oucar <hello@ucaronur.com>
2021-02-28 06:03:37 -0500

    drum kit has been completed!

--- MERN and Web/10- Advanced JS and DOM Manipulation, Drum Kit/js/index.js
+++ MERN and Web/10- Advanced JS and DOM Manipulation, Drum Kit/js/index.js
@@ -1,40 +1,40 @@
 const btns = document.querySelectorAll('button');
 
 const makeSound = function(key){
-    
+
     switch (key) {
         case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
+            let tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play(); 
             break;
 
         case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
+            let tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play(); 
             break;
 
         case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
+            let tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play(); 
             break;
 
         case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
+            let tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play(); 
             break;
 
         case "j":
-            var snare = new Audio("sounds/snare.mp3");
+            let snare = new Audio("sounds/snare.mp3");
             snare.play(); 
             break;
 
         case "k":
-            var crash = new Audio("sounds/crash.mp3");
+            let crash = new Audio("sounds/crash.mp3");
             crash.play(); 
             break;
 
         case "l":
-            var  kickBass = new Audio("sounds/kick-bass.mp3");
+            let  kickBass = new Audio("sounds/kick-bass.mp3");
             kickBass.play(); 
             break;
 
@@ -44,19 +44,33 @@ const makeSound = function(key){
     } // end switch
 } // end makeSound
 
+// ! MAKE THE BUTTONS MOVE
+const buttonAnimation = function(currKey){
+
+    let activeButton = document.querySelector(`.${currKey}`)
+
+    activeButton.classList.add('pressed');   // add class
+
+    setTimeout(function(){
+        activeButton.classList.remove('pressed');  // remove class
+    },100)
+
+} // end buttonAnimation();
 
 // ! DETECTS BUTTON CLICK
 for(btn of btns){
     btn.addEventListener('click', function(){
         //console.log(this);                      // ! do not use btn!
         makeSound(this.textContent);
+        buttonAnimation(this.textContent);
     });
 } // end for
 
 // ! DETECTS KEY PRESS
-document.addEventListener("keypress", function(e){                           
-    console.log(e.key);
+document.addEventListener("keypress", function(e){          // callback function, e is rendered when the document has keypress event                   
+    //console.log(e.key);
     makeSound(e.key);
+    buttonAnimation(e.key);
 })
 
 
