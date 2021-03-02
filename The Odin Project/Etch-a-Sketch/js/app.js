6fae189ffe791043ceeb8d0993825ca0eab63ad3
Onur Ucar <onurucar@iu.edu>
2021-03-01 23:51:47 -0500

    etch-a-sketch working version

--- The Odin Project/Etch-a-Sketch/js/app.js
+++ The Odin Project/Etch-a-Sketch/js/app.js
@@ -1,8 +1,9 @@
+// TODO: ADD TAIL 
 // =============== CREATING THE SQUARES ===============
 
-const container = document.querySelector(".container");          // select .container
+const container = document.querySelector(".container"); // select .container
 
-let squareCount = 16;                                            // TODO: interactivity
+let squareCount = 16; 
 let totalSquare = squareCount * squareCount;
 let color = `rgb(255,20,147)`;
 let isSelected = false;
@@ -10,7 +11,7 @@ let isSelected = false;
 for (let i = 0; i < totalSquare; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
-  square.textContent = i + 1;                                   //testing purposes only
+  //square.textContent = i + 1;                                   // ? testing purposes only
 
   container.appendChild(square);
 }
@@ -20,50 +21,47 @@ for (let i = 0; i < totalSquare; i++) {
 const rainbowColorBtn = document.querySelector(".rainbowColor"); // select color buttons
 const blackColorBtn = document.querySelector(".blackColor");
 
-blackColorBtn.addEventListener('click', function(){
-    color = 'black';
+blackColorBtn.addEventListener("click", function () {
+  color = "black";
+  isSelected = false;
 });
 
-const randomizeColor = function(){
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
+const randomizeColor = function () {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
 
-    return `rgb(${r}, ${g}, ${b})`;
-}
+  return `rgb(${r}, ${g}, ${b})`;
+};
 
-rainbowColorBtn.addEventListener('click', randomizeColor);
-rainbowColorBtn.addEventListener('click', function(e){
-    isSelected = !isSelected;
+rainbowColorBtn.addEventListener("click", randomizeColor);
+rainbowColorBtn.addEventListener("click", function (e) {
+  isSelected = true;
 });
-
-
 
 // =============== COLORING THE SQUARES ===============
 
 const colorize = function (colorInput) {
-
-    if(isSelected){
-        this.style = `background-color: ${randomizeColor()}`;
-    } else {
-        colorInput = color;
-        this.style = `background-color: ${colorInput}`;
-    }
+  if (isSelected) {
+    this.style = `background-color: ${randomizeColor()}`;
+  } else {
+    colorInput = color;
+    this.style = `background-color: ${colorInput}`;
+  }
 
   console.log("mouse on!");
 }; // end colorize()
 
-const squares = document.querySelectorAll("div");               // select ALL the divs
+const squares = document.querySelectorAll("div.square"); // select ALL the squares
 
 // Add event listener for each of the divs
 for (let square of squares) {
   square.addEventListener("mouseenter", colorize);
 }
 
-
 // ===============       CLEAR BUTTON     ===============
 
-const clearBtn = document.querySelector(".clearBtn");           // select the clear button
+const clearBtn = document.querySelector(".clearBtn"); // select the clear button
 
 const clear = function () {
   for (let square of squares) {
@@ -71,6 +69,49 @@ const clear = function () {
   }
 }; // end clear()
 
-clearBtn.addEventListener("click", clear);                      // add eventListener to clearBtn
+clearBtn.addEventListener("click", clear); // add eventListener to clearBtn
+
+// ===============         SIZING         ===============
+const selectBtn = document.querySelector("#selectSize"); // select the select button
+const squaresResize = document.querySelectorAll("section div"); // select the squares will be resized
+
+const initialize = function(sizeGiven, count){
+    const initializedSqures = squaresResize;
+    for (each of initializedSqures){
+        each.remove();
+    }
+
+    for (let i = 0; i < count; i++) {
+        const square = document.createElement("div");
+        square.classList.add(`${sizeGiven}`);
+        container.appendChild(square);
+      }
+
+}
 
 
+selectBtn.addEventListener("click", function (e) {
+  const inputs = document.querySelectorAll('input[name="choice"]');
+  let selectedValue;
+  for (const curr of inputs) {
+    if (curr.checked) {
+      selectedValue = curr.value;
+      
+        switch (selectedValue) {
+            case `square`:
+                initialize(selectedValue, 100);
+                break;
+            case `squareSmall`:
+                initialize(selectedValue, 600);
+                break;
+            case `squareXSmall`:
+                initialize(selectedValue, 3000);
+                 break;
+        
+            default:
+                break;
+        }
+    }
+  }
+  console.log(`square size set to: ${selectedValue}`); // ? testing purposes only
+});
