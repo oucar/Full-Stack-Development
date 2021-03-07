fb2676c373d0448b8a8966ca6318c397f5a340eb
oucar <hello@ucaronur.com>
2021-03-07 04:43:27 -0500

    last commit of the day

--- Web Developer/27- Async/2- Callbacks/app.js
+++ Web Developer/27- Async/2- Callbacks/app.js
@@ -1,7 +1,7 @@
 // ONLY ONE INSTRUCTION AT A TIME.
 
-console.log("Sending request to server!  ---- 1")
+console.log("Sending request to server!  ---- 1");
 setTimeout(() => {
-    console.log("Here is your data from the server... ---- 2")
-}, 3000)                                                            // THIS WILL BE PRINTED AFTER 3 SECONDS (not executed right away)
-console.log("I AM AT THE END OF THE FILE! ---- 3")
+  console.log("Here is your data from the server... ---- 2"); // ! THIS WILL BE PRINTED AFTER 3 SECONDS (not executed right away) 
+}, 3000);
+console.log("I AM AT THE END OF THE FILE! ---- 3");
