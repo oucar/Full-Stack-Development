df1ec4b0fe89f88ad28ded93d6cfefea1122219d
Onur Ucar <onurucar@iu.edu>
2021-03-07 04:26:31 -0500

    Callstack and Callbacks

---
+++ Web Developer/27- Async/1- Callstack/app.js
@@ -0,0 +1,15 @@
+// The Call Stack: The mechanism that javascript uses behind the scene. --> Calls multiple functions
+// Javascript runs one line at the same time --> SINGLE THREAD!
+// Stack : LIFO
+
+const multiply = (x, y) => x * y;
+
+const square = x => multiply(x, x);
+
+const isRightTriangle = (a, b, c) => (
+    square(a) + square(b) === square(c)             // ! returns true or false!
+)
+console.log("BEFORE")
+isRightTriangle(3, 4, 5)
+
+console.log("DONEEEE!")
