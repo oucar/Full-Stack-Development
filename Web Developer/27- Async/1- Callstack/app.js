5824d1b42f5f2ad05ca750bcb61e2150d3668af4
Onur Ucar <onurucar@MacBook-Pro.local>
2021-04-13 03:17:30 -0400

    first init in macOs

--- Web Developer/27- Async/1- Callstack/app.js
+++ Web Developer/27- Async/1- Callstack/app.js
@@ -7,7 +7,7 @@ const multiply = (x, y) => x * y;
 const square = x => multiply(x, x);
 
 const isRightTriangle = (a, b, c) => (
-    square(a) + square(b) === square(c)             // ! returns true or false!
+    square(a) + square(b) === square(c)         // ! returns true or false!
 )
 console.log("BEFORE")
 isRightTriangle(3, 4, 5)
