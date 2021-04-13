9f13aaeb86418d4f3e01e75758e19ed0eb852494
Onur Ucar <onurucar@MacBook-Pro.local>
2021-04-13 16:50:30 -0400

    async, promises, callbacks, call stack and await completed

--- Web Developer/27- Async/AsyncFunctions/app.js
+++ Web Developer/27- Async/AsyncFunctions/app.js
@@ -45,3 +45,62 @@ login('anything', 'corgifeetarecute')
         console.log("ERROR!")
         console.log(err)
     })
+
+
+//await works with async, not without
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('blue', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+    return "ALL DONE!"                      // this will be the resolved promise
+}
+
+async function printRainbow(){
+    await rainbow();                        // ! EXECUTE RAINBOW AND THEN CONSOLE.LOG BELOW!
+    console.log("rainbow is finished executing using the await keyboard!")
+}
+
+printRainbow();
+
+
+// Handling rejections
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        const delay = Math.floor(Math.random() * (4500)) + 500;
+        setTimeout(() => {
+            if (delay > 2000) {
+                reject('Connection Timeout :(')
+            } else {
+                resolve(`Here is your fake data from ${url}`)
+            }
+        }, delay)
+    })
+}
+
+
+async function makeTwoRequests() {              // rejecting using try and catch 
+    try {                                       // if it's error prone, go to catch statement
+        let data1 = await fakeRequest('/page1');
+        console.log(data1);
+        let data2 = await fakeRequest('/page2');
+        console.log(data2);
+    } catch (e) {                               // ! rejecting
+        console.log("CAUGHT AN ERROR!")
+        console.log("error is:", e)             // in this example, it will be "Connection Timeout :( from rejection state"
+    }
+
+}
