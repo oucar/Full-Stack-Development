2bc9bf68ba6b02347d128c4a2cf70f1ca5e25330
Onur Ucar <onurucar@MacBook-Pro.attlocal.net>
2021-05-28 13:27:17 -0400

    npm

---
+++ Web Developer/32- NPM Universe/shelter/index.js
@@ -0,0 +1,10 @@
+// node will look for index.js first!
+
+const blue = require('./blue')
+const sadie = require('./sadie')
+const janet = require('./janet')
+
+// array with 3 cat objects
+const allCats = [blue, sadie, janet]
+
+module.exports = allCats;
