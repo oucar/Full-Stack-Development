d18f840fbf65d7a4b42b0a86f57b0d75135e9c3c
Onur Ucar <onurucar@MacBook-Pro.attlocal.net>
2021-05-28 14:11:27 -0400

    Express

--- Web Developer/31- First Brush with Node.js/4- Process & Args/greeterForArgs.js
+++ Web Developer/31- First Brush with Node.js/4- Process & Args/greeterForArgs.js
@@ -1,9 +1,7 @@
+// node greeterForArgs.js onur ali veli 49 50
+
 const args = process.argv.slice(2);  // slice after 2
 
-// node greeterForArgs.js onur ali veli 49 50
 for ( let arg of args){
     console.log(`Hi there, ${arg}`);
 }
-
-
-// we could save both money and energy thanks to sustainability. it might be difficult for us to leave our habits such as using non-sustainable products
--- Web Developer/32- NPM Universe/LangGuessr/index.js
+++ Web Developer/32- NPM Universe/LangGuessr/index.js
@@ -3,12 +3,13 @@ const franc = require("franc");
 const langs = require("langs");
 const colors = require("colors")
 
+//  node index.js "THING WE WANT GOES HERE!"
 const input = process.argv[2];
 const langCode = franc(input);
 if (langCode === 'und') {
     console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red)
 } else {
     const language = langs.where("3", langCode);
-    console.log(`Our best guess is: ${language.name}`.green)
+    console.log(`Our best guess is: ${language.name}`)
 }
 
