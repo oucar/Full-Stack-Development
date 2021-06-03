fe044ce784611c57886643606dc7e7a90e4ff8e6
Onur Ucar <onurucar@MacBook-Pro.attlocal.net>
2021-06-03 02:02:40 -0400

    static assets in express

--- Web Developer/34- Dynamic HTML With Templating/App/index.js
+++ Web Developer/34- Dynamic HTML With Templating/App/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// import data.json
+const redditData = require('./data.json')
+
 // ! USE EJS
 app.set('view engine', 'ejs');
 
@@ -37,7 +40,14 @@ app.get('/cats', (req, res) => {
 app.get('/r/:subreddit/:num', (req,res) => {
     // ! take these from req.params
     const { subreddit, num} = req.params;
-    res.render('sub', {subreddit, num})
+    const data = redditData[subreddit]; // http://localhost:3000/r/soccer/41
+    console.log(data);
+    // ! make sure you spread the data so that we can easily access it in ejs files
+    if( data ) {
+        res.render('sub', {subreddit, num, ...data });
+    } else {
+        res.send(`No data has been found in ${subreddit}`)
+    } // end else 
 })
 
 // Error
