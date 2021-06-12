5ecd442f7565d6f682ead960b8aa644ec87c4e6c
Onur Ucar <onurucar@MacBook-Pro.attlocal.net>
2021-06-12 17:41:39 -0400

    create, read, show

--- Web Developer/35- REST/index.js
+++ Web Developer/35- REST/index.js
@@ -1,4 +1,5 @@
 // GET /comments - list all comments
+// NEW /comments/new - form to create a new comment
 // POST / comments - Create a new comment
 // GET /comments/:id - Get one comment (using ID)
 // PATCH /comments/:id - Update one comment
@@ -8,6 +9,7 @@
 const express = require('express');
 const app = express();
 const path = require ('path');
+const {v4 : uuid } = require('uuid'); // ? universally unique id --> uuid();
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.json());
@@ -17,19 +19,43 @@ app.set('view engine', 'ejs');
 // ! fake  comments for now (array of objects)
 const comments = [
     {
+        id: uuid(),
         username : 'Todd',
         comment : 'lolll',
     },
     {
+        id: uuid(),
         username: 'oucar',
         comment: 'hehe'
     }
 ]
 
+// !READ, GET
 app.get('/comments', (req, res) => {
     res.render('comments/index.ejs', {comments});
 })
 
+// ! NEW, just provides the form
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new.ejs');
+})
+
+// ! NEW, posts the information
+app.post('/comments', (req, res) => {
+    //console.log(req.body);
+    const {username, comment} = req.body;
+    comments.push({ username, comment, id: uuid() });
+    //res.send("it worked!");                 // ? use express redirects! 
+    res.redirect('/comments');                // (status code 302, found!)
+})
+
+// !SHOW route, information about one 
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find( c => c.id === id );
+    res.render('comments/show.ejs', {comment});
+
+})
 
 app.listen(3000, () => {
     console.log("ON PORT 3000!");
