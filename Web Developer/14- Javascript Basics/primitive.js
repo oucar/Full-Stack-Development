7c01d8d08aabc623884424b78861c77bf4c7c408
Onur Ucar <onurucar@MacBook-Pro-2.local>
2021-09-07 14:16:51 -0400

    primitive

--- Web Developer/14- Javascript Basics/primitive.js
+++ Web Developer/14- Javascript Basics/primitive.js
@@ -22,10 +22,19 @@ const hens = 4;
 // PREFIX - POSTFIX
 // ! prefix : If we’d like to increase a value and immediately use the result of the operator, we need the prefix form:
 let counter1 = 1;
-let a = counter1++; // (*) 
-alert(a);   // 1
+let x = counter1++; // (*) 
+alert(x);   // 1
 
 // ! postfix : If we’d like to increment a value but use its previous value, we need the postfix form:
 let counter2 = 1;
-let b = ++counter2;
-alert(b);   // 2
+let y = ++counter2;
+alert(y);   // 2
+
+let a = 1, b = 1;
+
+let c = ++a; // ?
+let d = b++; // ?
+
+// a=b=c=2, d=1
+
+
