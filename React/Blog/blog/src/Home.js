f2290bbe67ef543a9f28a34d695d3c5f73c50ead
Onur Ucar <onurucar@MacBook-Pro-3.attlocal.net>
2021-11-22 15:55:08 -0500

    useState hooks

--- React/Blog/blog/src/Home.js
+++ React/Blog/blog/src/Home.js
@@ -9,11 +9,20 @@ const Home = () => {
     }
 
     // useState Hook
+    // !NAME
     const [name, setName] = useState('mario');
 
     const changeName = (givenName) => {
         setName(givenName);
     }
+
+    // !AGE
+    const [age, setAge] = useState(13);
+
+    const changeAge = (givenAge) => {
+        setAge(givenAge);
+    }
+
  
     return (  
         <div className="home">
@@ -21,8 +30,12 @@ const Home = () => {
             {/* paranthesis invoke the function right away - handleClick(123) */}
             <button onClick={handleClick}>Click Me!</button>
 
-            <button onClick={() => changeName('onur')}>Change the name!</button>
+            <button onClick={() => {
+                changeName('onur');
+                changeAge(18);
+            }}>useState Hooks</button>
             <p> {name} </p>
+            <p> { age } </p>
         </div>
     );
 }
