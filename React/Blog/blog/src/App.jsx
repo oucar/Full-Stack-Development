92e52e64b17e39a463960966b71666324c30d666
Onur Ucar <onurucar@MacBook-Pro-3.attlocal.net>
2021-11-22 21:17:12 -0500

    react-router-dom

--- React/Blog/blog/src/App.jsx
+++ React/Blog/blog/src/App.jsx
@@ -1,19 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
 
   const title = "@oucar Blog"
-  const linkedin = "https://www.linkedin.com/in/oucar/"
 
   return (
-    <div className="App">
-    <Navbar/>
-      <div className="content">
-        <Home/>
-        <a href={linkedin}>My Linkedin</a>
+    <Router>
+      <div className="App">
+      <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home/>}>
+            </Route>
+          </Routes>            
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
