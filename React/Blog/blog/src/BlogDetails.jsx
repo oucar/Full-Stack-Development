4a80e40211b57a47deb32009986cdbe67cc4e037
Onur Ucar <onurucar@MacBook-Pro-3.attlocal.net>
2021-11-23 05:41:27 -0500

    Completed the BLOG prototype

--- React/Blog/blog/src/BlogDetails.jsx
+++ React/Blog/blog/src/BlogDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useNavigate } from "react-router-dom";
 
 const BlogDetails = () => {
 
@@ -7,6 +8,17 @@ const BlogDetails = () => {
     const { id } = useParams();
 
     const { data: blog, error, isPending } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+    const history = useNavigate();
+
+    const handleClick = () => {
+        fetch(`http://localhost:8000/blogs/${blog.id}`, {
+            method: "DELETE",
+        }).then(() => {
+            history('/');
+        })
+
+    }
 
     return ( 
         <div className="blog-details">
@@ -17,6 +29,8 @@ const BlogDetails = () => {
             <h2>{blog.title} - {id}</h2>
             <p>Written by @{blog.author}</p>
             <div>{blog.body}</div>
+
+            <button onClick={handleClick}>delete</button>
             </article>
 
         </div>
