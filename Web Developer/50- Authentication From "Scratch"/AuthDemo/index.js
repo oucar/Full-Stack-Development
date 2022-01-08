652d7baa72beb11c92c1b30720a4404276a108a1
Onur Ucar <onurucar@MacBook-Pro-3.local>
2022-01-08 06:30:07 -0500

    initial auth

--- Web Developer/50- Authentication From "Scratch"/AuthDemo/index.js
+++ Web Developer/50- Authentication From "Scratch"/AuthDemo/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 // Models
 const User = require('./models/user');
@@ -8,8 +9,25 @@ const User = require('./models/user');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// Auth
+const bcrypt = require('bcrypt');
+
+
+app.use(express.urlencoded({extended : true}));
+
 // Mongoose
-mongoose.connect('mongodb://localhost:27017/',)
+mongoose.connect('mongodb://localhost:27017/loginDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+app.get('/', (req, res) => {
+    res.send("Home Page");
+})
 
 // ! Secret page
 app.get('/secret', (req, res) => {
@@ -20,6 +38,43 @@ app.get('/secret', (req, res) => {
 app.get('/register', (req, res) => {
     res.render('register');
 })
+
+// make new user and save it -> async
+app.post('/register', async (req, res) => {
+
+    const{password, username} = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    // res.send(hash);
+
+    const user = new User({
+        username,
+        password: hash,
+    })
+    await user.save();
+
+    res.redirect('/');
+})
+
+// ! Login page
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+//
+app.post('/login', async (req, res) => {
+    const {password, username} = req.body;
+    const user = await User.findOne({ username })
+    // return true or false
+    const isValidUser = await bcrypt.compare(password, user.password);
+
+    if(isValidUser){
+        res.send(`Hello, ${user.username}, it's nice to see you.`);
+    } else{
+        res.send(`I don't know who you are.`);
+    }
+
+})
+
 
 
 
