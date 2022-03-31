bebbff954a12016fc1f36dd099914e2fe2f4379b
oucar <hello@ucaronur.com>
2022-03-31 00:32:34 -0400

    expsene date

--- React/React Complete/3- React Bacics/react-components/src/App.js
+++ React/React Complete/3- React Bacics/react-components/src/App.js
@@ -16,13 +16,8 @@ function App() {
     // this is the end state
     <div>
       <h1>Let's get started!</h1>
-      {/* attributes */}
+      {/* attributes, passing these to expense item */}
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
-      <ExpenseItem title={expenses[4].title} amount={expenses[4].amount} date={expenses[4].date}/>
-
     </div>
   );
 }
--- React/React Complete/3- React Bacics/react-components/src/components/ExpenseItem.js
+++ React/React Complete/3- React Bacics/react-components/src/components/ExpenseItem.js
@@ -1,12 +1,11 @@
-import { NativeDate } from 'mongoose';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
     return ( 
         <div className="expense-item">
             {/* date object cannot be outputted as is  */}
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
