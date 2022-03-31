e89de18583b6abf9b72f12e6e6ba4a7df52417be
Onur Ucar <onurucar@MacBook-Pro-4.attlocal.net>
2022-03-31 16:12:09 -0400

    use state in raect

--- React/React Complete/3- React Bacics/react-components/src/components/Expenses/ExpenseItem.js
+++ React/React Complete/3- React Bacics/react-components/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,29 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+
+    // useState is a hook, and only called inside a component 
+    let title = props.title;
+
+    const clickHandler = () => {
+        title = 'Updated.';
+    };
+
+
     return ( 
         <Card className="expense-item">
             {/* date object cannot be outputted as is  */}
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            {/* you just point, don't really execute it. it will be executed when an user clicks on it */}
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
