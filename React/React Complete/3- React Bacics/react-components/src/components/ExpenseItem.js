42ab420c75d71980193976e8ce6ae633f5574707
oucar <hello@ucaronur.com>
2022-03-31 00:05:15 -0400

    react stuff

--- React/React Complete/3- React Bacics/react-components/src/components/ExpenseItem.js
+++ React/React Complete/3- React Bacics/react-components/src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import { NativeDate } from 'mongoose';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-
-
+function ExpenseItem(props) {
 
     return ( 
         <div className="expense-item">
             {/* date object cannot be outputted as is  */}
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
