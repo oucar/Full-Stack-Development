64880138def44212ed534aa7f9eb859065284c4c
oucar <hello@ucaronur.com>
2022-03-31 15:24:48 -0400

    organizing the code

---
+++ React/React Complete/3- React Bacics/react-components/src/components/UI/Card.js
@@ -0,0 +1,14 @@
+import './Card.css';
+
+export default function Card(props) {
+
+    // Using custom stuff with inner components might be painful
+    // Use this logic to dynamically get the classname. Card component now has the 'card' class and whatever expenses you use there.
+    // <Card className="expense-item"> --- className is a props name of the Card component.
+    const classes = 'card ' + props.className;
+    return (
+        //   We are using props.children so that we can use custom component inside Card
+        //   See ExpenseItem.js
+        <div className={classes}>{props.children}</div>
+    )
+}
